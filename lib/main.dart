4a9a93f75ea6f8fee681072c1af7c8afa500d934
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2025-05-25 13:15:56 +0300

    initial project , Add required dependencies

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'core/app_theme.dart';
+
+void main() {
+  runApp(PostsApp());
+}
+
+class PostsApp extends StatelessWidget {
+  const PostsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
+      title: 'Posts App',
+      home: Scaffold(appBar: AppBar(title: Text('Posts'))),
+    );
+  }
+}

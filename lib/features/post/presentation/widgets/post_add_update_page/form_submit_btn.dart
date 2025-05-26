4067ea8be496321e3fd080d44482398458c709b1
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2025-05-27 00:28:05 +0300

    Create Post add update page in presentation layer - part 2

---
+++ lib/features/post/presentation/widgets/post_add_update_page/form_submit_btn.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/app_theme.dart';
+
+class FormSubmitBtn extends StatelessWidget {
+  const FormSubmitBtn({
+    super.key,
+    required this.onPressed,
+    required this.isUpdatePost,
+  });
+
+  final void Function() onPressed;
+  final bool isUpdatePost;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: onPressed,
+      icon: isUpdatePost ? Icon(Icons.edit) : Icon(Icons.add),
+      label: Text(isUpdatePost ? 'Update' : 'Add'),
+      style: ElevatedButton.styleFrom(backgroundColor: primaryColor),
+    );
+  }
+}
